f3817b0cda706b4fb5ff5effca0f1ab94985af64
dunphyo2020 <56635458+dunphyo2020@users.noreply.github.com>
2019-12-11 13:17:44 +0000

    Final version of app scripts

--- scripts/app.js
+++ scripts/app.js
@@ -27,9 +27,10 @@ function renderSupplier(doc){
 //new realtime caller
 db.collection('Suppliers').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
+    var userId = auth.currentUser.uid;
     console.log(changes);
     changes.forEach(change => {
-        if(change.type == 'added'){
+        if(change.type == 'added' && change.doc.data().User == userId){
             renderSupplier(change.doc)
         }else if(change.type == 'remove'){
            //add removal code 
@@ -38,18 +39,109 @@ db.collection('Suppliers').onSnapshot(snapshot => {
 })
 
 supplierForm.addEventListener('submit', (e) => {
-
+    var userId = auth.currentUser.uid;
+    console.log(userId);
     e.preventDefault();
     db.collection('Suppliers').add({
         Name: supplierForm.addName.value,
         RepName: supplierForm.addRep.value,
         Email: supplierForm.addEmail.value,
-        Account: supplierForm.addAccount.value
+        Account: supplierForm.addAccount.value,
+        User: userId
     })
     supplierForm.addName.value = '';
     supplierForm.addRep.value = '';
     supplierForm.addEmail.value = '';
     supplierForm.addAccount.value = '';
 })
+
+function addRecipe(){
+    //saving registration details
+        const recipeForm= document.querySelector('#add-recipe');
+        console.log("Storing...");
+        db.collection('Recipes').add({
+            Name: recipeForm.name.value,
+            Meal: document.querySelector('input[name="Meal"]:checked').value,
+            Method: recipeForm.method.value,
+            Time: recipeForm.time.value,
+            Cuisine: document.querySelector('input[name="Cuisine"]:checked').value,
+            Complexity: document.querySelector('input[name="Complexity"]:checked').value,
+        })
+       console.log("Complete.");
+    
+       //window.location.href='./recipes.html';
+    }
+    
+    
+    // create element & render cafe
+    function renderRecipes(doc){
+        console.log('Rendering data...');
+        let li = document.createElement('li');
+        let name = document.createElement('span');
+        let meal = document.createElement('span');
+        let method = document.createElement('span');
+        let time = document.createElement('span');
+        let cuisine = document.createElement('span');
+        let complex = document.createElement('span');
+    
+    
+        li.setAttribute('id', doc.id);
+        name.textContent = doc.data().Name;
+        meal.textContent = doc.data().Meal;
+        method.textContent = 'Method: ' + doc.data().Method;
+        time.textContent = 'Time: '+ doc.data().Time + 'mins';
+        cuisine.textContent = 'Cusine: '+  doc.data().Cuisine;
+        complex.textContent = 'Complexity: ' + doc.data().Complexity;
+    
+        li.appendChild(name);
+        li.appendChild(meal);
+        li.appendChild(method);
+        li.appendChild(time);
+        li.appendChild(cuisine);
+        li.appendChild(complex);
+    
+        list.appendChild(li);
+    }
+    
+    function loadRecipes(){
+        const list = document.getElementById('#recipeList');
+        // getting data
+        console.log('Retrieving data...');
+        db.collection('Recipes').get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+                renderRecipes(doc);
+            });
+        });
+        console.log('Complete.');
+    }
+    
+    function specificRecipes(a, b, c){
+        removeRecipes();
+        const list = document.getElementById('#recipeList');  
+    
+        // getting data
+        console.log('Filtering data...');
+        db.collection('Recipes').where(a, b, c).get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+                renderRecipes(doc);
+            });
+         });
+        console.log('Complete.');
+    }
+    
+    function removeRecipes(doc){
+    
+        console.log('Removing data...'); 
+        db.collection('Recipes').get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+                let x = document.getElementById(doc.id);
+                if(x != null){
+                    x.parentNode.removeChild(x);
+                }
+            });
+        });
+    ;
+        console.log('Removed'); 
+    }
 
 
